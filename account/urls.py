997d5de0d45bf845999b8bddd6c782682596ab0b
struhanets <posseydon87@gmail.com>
2024-12-04 19:09:39 +0100

    added urls to account app by routing

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,14 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from account import views
+
+router = routers.DefaultRouter()
+
+router.register("profiles", views.ProfileViewSet)
+router.register("posts", views.PostViewSet)
+router.register("comments", views.CommentViewSet)
+
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "account"
-
-urlpatterns = []

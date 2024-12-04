308bf3156c18ec6a031b731bedb99ecb8ce80d61
struhanets <posseydon87@gmail.com>
2024-12-04 19:08:52 +0100

    added views to account app

--- account/views.py
+++ account/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from account.models import Profile, Post, Comment
+from account.serializers import ProfileSerializer, PostSerializer, CommentSerializer
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer

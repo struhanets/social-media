29049904ffe3ff77f16ded519997eaea661a3a82
struhanets <posseydon87@gmail.com>
2024-12-07 17:36:38 +0100

    Implemented profile filtering by last_name & first_name

--- account/views.py
+++ account/views.py
@@ -24,6 +24,21 @@ class ProfileViewSet(viewsets.ModelViewSet):
 
         return ProfileSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+
+        last_name = self.request.query_params.get("last_name")
+        first_name = self.request.query_params.get("first_name")
+        if last_name:
+            return queryset.filter(last_name__icontains=last_name)
+        elif first_name:
+            return queryset.filter(first_name__icontains=first_name)
+
+        if self.action == "list":
+            queryset = queryset.select_related()
+
+        return queryset
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()

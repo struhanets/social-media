318717c257ca23b141761a22aa0a170b87165211
struhanets <posseydon87@gmail.com>
2024-12-09 11:23:11 +0100

    registered models in admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from account.models import Profile, Post, Comment, Reaction
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Reaction)
+admin.site.register(Comment)
